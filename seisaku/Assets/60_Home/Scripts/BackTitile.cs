ae7d300b3557bbb853bef6f68ccd3b04f0bd7182
DESKTOP-5OIA18N\yuday <bisamya349@gmail.com>
2023-01-29 15:11:53 +0900

    no message

--- seisaku/Assets/60_Home/Scripts/BackTitile.cs
+++ seisaku/Assets/60_Home/Scripts/BackTitile.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using SoundManager;
+using PublicUI;
+
 
 public class BackTitile : MonoBehaviour
 {
     [SerializeField]
     private GameObject sound;
-    public void TitileBack()
+    public async void TitileBack()
     {
         sound.GetComponent<SoundSystems>().PlaySE(0);
+        var fade = GameObject.Find("FadeUI");
+        fade.GetComponent<FadeSystems>().FadeOut();
+        await Task.Delay(3000);
         SceneManager.LoadScene("TitleScene");
     }
 }
